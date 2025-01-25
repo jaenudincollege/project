30fb8a4a340f14927d3051813811d942130d1eaf
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-25 13:09:20 +0700

    Fetch movie data on App component mount

--- movies-app/src/App.tsx
+++ movies-app/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import Search from "./components/Search";
+import { searchMovie } from "./services/movieService";
 
 const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await searchMovie("Batman");
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Search />
