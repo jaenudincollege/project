4e53da77f486543eac5b070343b5421c69964138
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-25 13:08:22 +0700

    Add searchMovie function to movieService

--- movies-app/src/services/movieService.ts
+++ movies-app/src/services/movieService.ts
@@ -1,2 +1,12 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
 const BASE_POSTER_URL = "https://image.tmdb.org/t/p/original";
+
+export const searchMovie = async (query: string) => {
+  const response = await fetch(
+    `${BASE_URL}search/movie?api_key=${
+      import.meta.env.VITE_API_KEY
+    }&query=${query}`
+  );
+  const data = await response.json();
+  return data.results;
+};
