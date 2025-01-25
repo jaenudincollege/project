168ab8855b24ed98be4e76b99b2ffd9d892307e7
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-25 13:34:08 +0700

    Update BASE_POSTER_URL to use w500 and export it

--- movies-app/src/services/movieService.ts
+++ movies-app/src/services/movieService.ts
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const BASE_POSTER_URL = "https://image.tmdb.org/t/p/original";
+export const BASE_POSTER_URL = "https://image.tmdb.org/t/p/w500";
 
 export const searchMovie = async (query: string) => {
   const response = await fetch(
