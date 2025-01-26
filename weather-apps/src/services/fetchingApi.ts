5333c9a68db648c046b3bfffcb8463a2fb9753b6
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-26 09:17:08 +0700

    add optional parameter

--- weather-apps/src/services/fetchingApi.ts
+++ weather-apps/src/services/fetchingApi.ts
@@ -1,6 +1,11 @@
-const getWeather = async (query: string) => {
+const getWeather = async (query: string, signal?: AbortSignal) => {
   const res = await fetch(
-    `${import.meta.env.VITE_BASE_URL}?key=${import.meta.env.VITE_API_KEY}&q=${query}`
+    `${import.meta.env.VITE_BASE_URL}?key=${
+      import.meta.env.VITE_API_KEY
+    }&q=${query}`,
+    {
+      signal,
+    }
   );
   const data = await res.json();
   return data;
