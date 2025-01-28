e8252f4592fb78aa782637eae44c0ea3977e5d8e
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-28 11:51:12 +0700

    feat: refactor button as children and accept props

--- personal-note/src/components/Button.jsx
+++ personal-note/src/components/Button.jsx
@@ -1,5 +1,5 @@
-function Button({ btnText }) {
-  return <button>{btnText}</button>;
+function Button({ children, className }) {
+  return <button className={className}>{children}</button>;
 }
 
 export default Button;
