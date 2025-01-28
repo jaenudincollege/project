913da2746c8d87ee1824af832b24a168dd1b6216
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-28 11:16:54 +0700

    feat: add state and integrate Input in Header component

--- personal-note/src/components/Header.jsx
+++ personal-note/src/components/Header.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "../styles/header.css";
+import Input from "./Input";
 
 function Header() {
+  const [query, setQuery] = useState("");
+  console.log(query);
+
   return (
     <header className="header">
       <div className="header-container">
         <h2 className="header-logo">Logo</h2>
-        <input type="text" />
+        <Input value={query} onChange={(e) => setQuery(e.target.value)} />
       </div>
     </header>
   );
