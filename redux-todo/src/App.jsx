ff8fc999ccad568ba25066ca035e93e3faf882e1
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-03-11 16:00:11 +0700

    Create Main styled layout in App

--- redux-todo/src/App.jsx
+++ redux-todo/src/App.jsx
@@ -1,11 +1,20 @@
 import GlobalStyle from "./styles/GlobalStyles";
 import Todo from "./features/Todo";
+import styled from "styled-components";
+
+const Main = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 44px;
+`;
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Todo />
+      <Main>
+        <Todo />
+      </Main>
     </>
   );
 };
